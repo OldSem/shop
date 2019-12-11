88ff2532beeb6c5cbd6da79fd4975d9d346be0cd
OldSem <old_sem@meta.ua>
2019-12-11 12:53:20 +0200

    rmv media dir

--- shop/forms.py
+++ shop/forms.py
@@ -1,47 +1,74 @@
 from django import forms
 from .models import Good, Image,Category,Post,Theory,Order
 
+
 class GoodForm(forms.ModelForm):
+
     title = forms.CharField(max_length=128)
     description = forms.CharField(widget=forms.Textarea)
 
     class Meta:
+
         model = Good
-        fields = ('title', 'description','category','price',)
-        labels = {'title': 'Название', 'description': 'Описание', 'category': 'Категория', 'price': 'Цена',  }
+        fields = ('title', 'description', 'category', 'price')
+        labels = {'title': 'Название',
+                  'description': 'Описание',
+                  'category': 'Категория',
+                  'price': 'Цена',
+                  }
+
 
 class ImageForm(forms.ModelForm):
+
     image = forms.ImageField(label='Image')
+
     class Meta:
+
         model = Image
         fields = ('image', 'good' )
 
 
 class CategoryForm(forms.ModelForm):
+
     class Meta:
+
         model = Category
         fields = ('name', 'parent',)
-        labels = {'name': 'Название', 'parent': 'Надкатегория',}
+        labels = {'name': 'Название',
+                  'parent': 'Надкатегория',
+                  }
         values = {"save": 'Добавить'}
 
+
 class QuantityForm(forms.Form):
+
     quantity = forms.IntegerField()
 
 
-
 class OrderForm(forms.ModelForm):
+
     class Meta:
+
         model = Order
-        fields = ('user', 'phone', 'city','street','build','aptmt','payment')
-        labels = {'user': 'Имя Фамилия', 'phone': 'Телефон', 'city': 'Населенный пункт', 'street': 'Улица','build': 'Дом','aptmt': 'Квартира','payment': 'Оплата'}
+        fields = ('user', 'phone', 'city', 'street', 'build', 'aptmt',
+                  'payment')
+        labels = {'user': 'Имя Фамилия',
+                  'phone': 'Телефон',
+                  'city': 'Населенный пункт',
+                  'street': 'Улица',
+                  'build': 'Дом',
+                  'aptmt': 'Квартира',
+                  'payment': 'Оплата'}
         values = {"save": 'Оформить заказ'}
         widgets = {
-            'payment': forms.RadioSelect(choices=[(1,'Карта Приват'),(2,'Наложенным платежом')])}
-
+            'payment': forms.RadioSelect(choices=[(1,'Карта Приват'),
+                                        (2,'Наложенным платежом')])}
 
 
 class TheoryForm(forms.ModelForm):
+
     class Meta:
+
         model = Theory
         fields = ('name','parent','text')
         labels = {'name': 'Тема','parent': 'Надтема','text':'Тело',}
--- shop/models.py
+++ shop/models.py
@@ -8,14 +8,10 @@ from sorl.thumbnail import ImageField
 
 class Category(models.Model):
 
-
     name = models.CharField(max_length=200)
     slug = models.SlugField()
-    parent = models.ForeignKey('self',
-                               blank=True,
-                               null=True,
-                               related_name='children',
-                               on_delete=models.PROTECT)
+    parent = models.ForeignKey('self', blank=True, null=True,
+        related_name='children', on_delete=models.PROTECT)
 
     class Meta:
         # enforcing that there can not be two categories under a parent with same slug
@@ -31,7 +27,7 @@ class Category(models.Model):
         if not self.slug:
             slug = slugify(unidecode(self.name))
             while True:
-                if Category.objects.filter(slug=slug).count()>0:
+                if Category.objects.filter(slug=slug).count() > 0:
                     slug = slug + '-'
                 else:
                     self.slug = slug
@@ -49,18 +45,20 @@ class Category(models.Model):
 
 class Good(models.Model):
 
+    title = models.CharField(max_length=200, blank=True, null=True)
+    price = models.FloatField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    category = models.ForeignKey(Category,blank=True, null=True,
+                                 on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='owner', null=True,
+                             on_delete=models.CASCADE)
 
-    title = models.CharField(max_length=200,blank=True,null=True,)
-    price = models.FloatField(blank=True,null=True,)
-    description = models.TextField(blank=True,null=True,)
-    category = models.ForeignKey(Category,blank=True,null=True,on_delete=models.CASCADE)
-    user = models.ForeignKey(User,related_name = 'owner',null=True,on_delete=models.CASCADE)
-    
     def __str__(self):
-        if self.title!=None:
+        if self.titleis is not None:
             return self.title
         else:
             return ''
+
 
 def get_image_filename(instance, filename):
     title = instance.good.title
@@ -69,35 +67,41 @@ def get_image_filename(instance, filename):
 
 
 class Image (models.Model):
-    good = models.ForeignKey(Good, default=None,on_delete=models.CASCADE,null=True,blank=True,related_name='images')
+
+    good = models.ForeignKey(Good, default=None, on_delete=models.CASCADE,
+                             null=True, blank=True, related_name='images')
     image = ImageField(upload_to='shop/%Y/%m/%d',
                               verbose_name='Image')
 
 
 class Order(models.Model):
-    owner = models.ForeignKey(User,null=True,on_delete=models.CASCADE)
-    user = models.CharField(max_length=50, default=None,null=True)
-    phone = models.CharField(max_length=15, default=None,null=True)
-    city = models.CharField(max_length=50, default=None,null=True)
-    street = models.CharField(max_length=50, default=None,null=True)
-    build = models.CharField(max_length=15, default=None,null=True)
-    aptmt = models.CharField(max_length=15, default=None,null=True,blank=True)
-    payment = models.IntegerField( default=None,null=True,blank=True)
-    total = models.FloatField(blank=True,null=True,)
-    status = models.CharField(max_length=50, default=None,null=True)
+
+    owner = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    user = models.CharField(max_length=50, default=None, null=True)
+    phone = models.CharField(max_length=15, default=None, null=True)
+    city = models.CharField(max_length=50, default=None, null=True)
+    street = models.CharField(max_length=50, default=None, null=True)
+    build = models.CharField(max_length=15, default=None, null=True)
+    aptmt = models.CharField(max_length=15, default=None, null=True,
+                             blank=True)
+    payment = models.IntegerField( default=None, null=True, blank=True)
+    total = models.FloatField(blank=True, null=True)
+    status = models.CharField(max_length=50, default=None, null=True)
+
 
 class Basket(models.Model):
-    order = models.ForeignKey(Order,on_delete=models.CASCADE)
-    good = models.ForeignKey(Good,on_delete=models.CASCADE)
+
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    good = models.ForeignKey(Good, on_delete=models.CASCADE)
     quantity = models.IntegerField()
 
 
-
-
 class Theory(models.Model):
+
     name = models.CharField(max_length=200)
     slug = models.SlugField()
-    parent = models.ForeignKey('self', blank=True, null=True, related_name='children',on_delete=models.PROTECT)
+    parent = models.ForeignKey('self', blank=True, null=True,
+        related_name='children', on_delete=models.PROTECT)
     text = models.TextField()
 
     class Meta:
@@ -107,14 +111,14 @@ class Theory(models.Model):
 
         # __str__ if you are using python 2
 
-        unique_together = ('slug', 'parent',)
+        unique_together = ('slug', 'parent')
         verbose_name_plural = "theory"
 
     def save(self, *args, **kwargs):
         if not self.slug:
             slug = slugify(unidecode(self.name))
             while True:
-                if Theory.objects.filter(slug=slug).count()>0:
+                if Theory.objects.filter(slug=slug).count() > 0:
                     slug = slug + '-'
                 else:
                     self.slug = slug
@@ -128,7 +132,6 @@ class Theory(models.Model):
             full_path.append(k.name)
             k = k.parent
         return ' -> '.join(full_path[::-1])
-
 
 
 class Post(models.Model):
