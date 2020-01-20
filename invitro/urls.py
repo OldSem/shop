9f6d81a0a77043fc25dfc97e83198ee6ca6126e5
OldSem <old_sem@meta.ua>
2020-01-20 17:08:15 +0200

    view images

--- invitro/urls.py
+++ invitro/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     url(r'^signup/$', core_views.signup, name='signup'),
     url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
